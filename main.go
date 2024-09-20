9d080e9420464633d91137b988a85589050ad746
Jakub Borusewicz <jakub.borusewicz@gmail.com>
2024-09-20 18:33:18 +0200

    Wip

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import "path"
 import "cuelang.org/go/cue/load"
 import "cuelang.org/go/cmd/cue/cmd"
 import "cuelang.org/go/cue/cuecontext"
-import "log"
 func main() {
   ctx := cuecontext.New()
   files := os.Args[1:]
@@ -18,17 +17,21 @@ func main() {
     insts := load.Instances([]string{file}, nil)
     v := ctx.BuildInstance(insts[0])
     fmt.Printf("l1%v\n", v)
-    export_command, _ := cmd.New([]string{"export"})
+
+//     command_string := fmt.Sprintf("export %[1]v --out yaml --outfile %[2]v  --force", file, no_cue_filename)
+//     command_string := "export other_example.cue --out yaml --outfile other_example  --force"
+    command_string := "export"
+    export_command, _ := cmd.New([]string{command_string, "other_example.cue", "--out", "yaml", "--outfile", "other_example", "--force"})
     res := export_command.Execute()
     fmt.Printf("l2%v\n", res)
 //     ctx.CompileString()
-    f, err := os.OpenFile(no_cue_filename, os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        log.Fatal(err)
-    }
-    
+//     f, err := os.OpenFile(no_cue_filename, os.O_CREATE|os.O_WRONLY, 0644)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//
 //     f.WriteString(v)
-    defer f.Close()
+//     defer f.Close()
 
   }
 
