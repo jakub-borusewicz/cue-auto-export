19e5c6e2f59dd7885034ac5eed76ebec0fcf178e
jakubborusewicz <jakub.borusewicz@ulam.io>
2025-02-13 12:45:07 +0100

    Parametrization

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import S "strings"
 import "path"
 import "cuelang.org/go/cmd/cue/cmd"
 import "fmt"
+import "strings"
+import "github.com/spf13/cobra"
+import "regexp"
+import cartesian "github.com/schwarmco/go-cartesian-product"
+
 
 var extension_out_map = map[string]string{
     ".json": "json",
@@ -20,24 +25,123 @@ var extension_out_map = map[string]string{
 }
 
 func main() {
-  files := os.Args[1:]
-  run_for_files(files)
+  var parametrizationMapFlag map[string]string
+  cmd := &cobra.Command{
+     Use: "cue-auto-export",
+     Run: func(cmd *cobra.Command, args []string) {
+         parametrizationMap := map[string][]string{}
+         for k, v := range parametrizationMapFlag {
+              parametrizationMap[k] = strings.Split(v, ",")
+         }
+         // todo pass parametrizationMap to run_for_files and use it
+         fmt.Printf("parametrizationMap: %v\n", parametrizationMap)
+         run_for_files(args, parametrizationMap)
+     },
+  }
+
+  cmd.PersistentFlags().StringToStringVarP(&parametrizationMapFlag, "parametrize", "p", nil, "parametrization map string")
+  if err := cmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
 
-func run_for_files(files []string) {
+type FileExportParameters struct {
+    cue_filename string
+    no_cue_filename string
+    out_format string
+    tags_map map[string]string
+}
+
+func cartesianProduct(parametrizationMap map[string][]string) []map[string]string {
+    keys := []string{}
+    values := [][]interface{}{}
+    for k, v := range parametrizationMap {
+        keys = append(keys, k)
+        var interfaceSlice []interface{}
+        for _, value := range v {
+            interfaceSlice = append(interfaceSlice, value)
+        }
+        values = append(values, interfaceSlice)
+    }
+
+    c := cartesian.Iter(values...)
+    result := []map[string]string{}
+
+    for tuple := range c {
+        tuple_map := map[string]string{}
+        for i, key := range keys {
+            tuple_map[key] = tuple[i].(string)
+        }
+        result = append(result, tuple_map)
+    }
+
+    return result
+}
+
+
+func run_for_files(files []string, parametrizationMap map[string][]string) {
     for _, file := range files {
         fmt.Println("Exporting file", file)
-        no_cue_filename := S.TrimSuffix(file, ".cue")
-        extension := path.Ext(no_cue_filename)
-        out_format, extension_mapped := extension_out_map[extension]
+        filename_parse_result := parse_filename(file)
+        out_format, extension_mapped := extension_out_map[filename_parse_result.extension]
         if !extension_mapped {
            out_format = "text"
         }
-        export_command, _ := cmd.New([]string{"export", file, "--out", out_format, "--outfile", no_cue_filename, "--force"})
-        result := export_command.Execute()
-        if result != nil {
-            fmt.Println("Error - could not export file", file)
-            panic(result)
+        if len(filename_parse_result.parametrizationVariables) == 0 {
+            export_file(FileExportParameters{file, filename_parse_result.no_cue_filename, out_format, map[string]string{}})
+        } else {
+            fileParamMap := map[string][]string{}
+            for _, paramVar := range filename_parse_result.parametrizationVariables {
+                paramValueList, is_mapped := parametrizationMap[paramVar]
+                if !is_mapped {
+                    fmt.Printf("Error - cannot match placeholder {%s} with any provided parameters while exporting file {%s}\n", paramVar, file)
+                    panic("")
+                }
+                fileParamMap[paramVar] = paramValueList
+            for _, tags_map := range cartesianProduct(fileParamMap) {
+                result_file_name := filename_parse_result.no_cue_filename
+                for k, v := range tags_map {
+                    result_file_name = S.ReplaceAll(result_file_name, "{" + k + "}", v)
+
+                }
+                export_file(FileExportParameters{file, result_file_name, out_format, tags_map})
+
+            }
+            }
         }
     }
+}
+
+
+func export_file(params FileExportParameters) {
+    command_list := []string{"export", params.cue_filename, "--out", params.out_format, "--outfile", params.no_cue_filename, "--force"}
+    for k, v := range params.tags_map {
+        command_list = append(command_list, []string{"--inject", k + "=" + v}...)
+    }
+    export_command, _ := cmd.New(command_list)
+    result := export_command.Execute()
+    if result != nil {
+        fmt.Println("Error - could not export file", params.cue_filename)
+        panic(result)
+    }
+}
+
+
+type FilenameParseResult struct {
+    no_cue_filename string
+    extension string
+    parametrizationVariables []string
+}
+
+func parse_filename(filename string) FilenameParseResult {
+    no_cue_filename := S.TrimSuffix(filename, ".cue")
+    extension := path.Ext(no_cue_filename)
+    r, _ := regexp.Compile("{(.+?)}")
+    parametrizationVariablesMatches := r.FindAllStringSubmatch(no_cue_filename, -1)
+    parametrizationVariables := []string{}
+    for _, match := range parametrizationVariablesMatches {
+        parametrizationVariables = append(parametrizationVariables, match[1])
+    }
+    return FilenameParseResult{no_cue_filename, extension, parametrizationVariables}
 }
--- main_test.go
+++ main_test.go
@@ -3,14 +3,15 @@ package main
 import "testing"
 import "os"
 import "github.com/stretchr/testify/assert"
-
+// import "reflect"
+// import "sort"
 const expected_result_file_content = `{
     "a": 1
 }
 `
 
-func TestRunForFiles(t *testing.T) {
-    //given
+func TestRunForFilesWithoutParametrization(t *testing.T) {
+    // given
     tmp_dir := t.TempDir()
     cue_file_content := "a: 1\n"
 
@@ -21,7 +22,7 @@ func TestRunForFiles(t *testing.T) {
     f.WriteString(cue_file_content)
 
     // when
-    run_for_files([]string{cue_file_path})
+    run_for_files([]string{cue_file_path}, map[string][]string{})
 
     // then
     _, err1 := os.Stat(expected_result_file_path)
@@ -29,3 +30,81 @@ func TestRunForFiles(t *testing.T) {
     actual_json_file_content, _ := os.ReadFile(expected_result_file_path)
     assert.Equal(t, expected_result_file_content, string(actual_json_file_content))
 }
+
+
+const expected_result_file_content1 = `{
+    "a": "dev"
+}
+`
+
+const expected_result_file_content2 = `{
+    "a": "prod"
+}
+`
+
+func TestRunForFilesWithParametrization(t *testing.T) {
+    //given
+    tmp_dir := t.TempDir()
+    cue_file_content := "a: _ @tag(env)\n"
+
+    parametrizationMap := map[string][]string{
+        "env": []string{"dev", "prod"},
+    }
+
+    cue_file_path := tmp_dir + "/some_file_{env}.json.cue"
+    expected_result_file_path1 := tmp_dir + "/some_file_dev.json"
+    expected_result_file_path2 := tmp_dir + "/some_file_prod.json"
+    f, _ := os.Create(cue_file_path)
+    defer f.Close()
+    f.WriteString(cue_file_content)
+
+    // when
+    run_for_files([]string{cue_file_path}, parametrizationMap)
+
+    // then
+    _, err1 := os.Stat(expected_result_file_path1)
+    assert.Nil(t, err1)
+    actual_json_file_content1, _ := os.ReadFile(expected_result_file_path1)
+    assert.Equal(t, expected_result_file_content1, string(actual_json_file_content1))
+
+    _, err2 := os.Stat(expected_result_file_path2)
+    assert.Nil(t, err2)
+    actual_json_file_content2, _ := os.ReadFile(expected_result_file_path2)
+    assert.Equal(t, expected_result_file_content2, string(actual_json_file_content2))
+}
+
+
+func TestParseFilename(t *testing.T) {
+    // given
+    filename := "/some_file_{env}.json.cue"
+
+    // when
+    result := parse_filename(filename)
+
+    // then
+    assert.Equal(t, "/some_file_{env}.json", result.no_cue_filename)
+    assert.Equal(t, ".json", result.extension)
+    assert.Equal(t, []string{"env"}, result.parametrizationVariables)
+}
+
+func TestCartesianProduct(t *testing.T) {
+    // given
+    input := map[string][]string{
+        "z": []string{"a", "b"},
+        "y": []string{"1", "2"},
+    }
+
+    // when
+    result := cartesianProduct(input)
+
+    // then
+    expected_result := []map[string]string{
+        {"z": "a", "y": "1"},
+        {"z": "a", "y": "2"},
+        {"z": "b", "y": "1"},
+        {"z": "b", "y": "2"},
+    }
+    assert.ElementsMatch(t, result, expected_result)
+}
+
+
